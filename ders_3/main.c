0bd29b81fe5b4f77b793d5bcebf331b7aa7bdf9f
gizem <gizem@gizem-Inspiron-5521.(none)>
2013-04-29 13:34:12 +0300

    uygulama dersi kodu eklendi3

---
+++ ders_3/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int fonk(int, int);
+int main()
+{
+    int i;
+    int sayi1;
+    int sayi2;
+
+    for(i = 0; i < 5; i++){
+        scanf("%d %d",&sayi1, &sayi2);
+
+        if(fonk(sayi1, sayi2))
+            printf("%d %d nin katidir\n",sayi2, sayi1);
+        else
+            printf("%d %d nin kati degildir.\n",sayi2, sayi1);
+    }
+    return 0;
+}
+int fonk(int sayi1, int sayi2){
+    if(sayi2 % sayi1==0)
+        return 1;
+    else
+        return 0;
+}
