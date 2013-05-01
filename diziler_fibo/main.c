104cd3aab9c960ec4266a68f30414a4cdc150554
gizem <gizem@gizem-Inspiron-5521.(none)>
2013-05-01 18:29:08 +0300

    fibonakki

---
+++ diziler_fibo/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define BOYUT 20
+
+int fibonakki(int );
+void fiboDizi(int[],int);
+int main()
+{
+   //int i;
+    int dizi[BOYUT];
+    fiboDizi(dizi,BOYUT);
+
+    //for(i=0; i<BOYUT; i++)
+
+        printf("%d\n", dizi[5]);
+
+    return 0;
+}
+int fibonakki(int m) {
+int toplam=0;
+int ilk=0;
+int son=1;
+int i;
+    if(m<2)
+        return 1;
+
+else {
+    for(i=1; i<=m; i++) {
+        toplam=ilk+son;
+        ilk=son;
+        son=toplam;
+    }
+}
+    return toplam;
+}
+
+void fiboDizi(int dizi[],int boyut){
+
+    int i;
+        for(i=0; i<boyut; i++){
+            dizi[i]=fibonakki(i+1);
+    }
+
+}
